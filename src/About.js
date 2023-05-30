5f7af2f16ea728a7da615632406516180c359931
Daniel Quinn <dandidit.quinn@gmail.com>
2023-05-29 22:07:53 -0700

    done

--- src/About.js
+++ src/About.js
@@ -1,11 +1,17 @@
 import { Component } from "react";
 
-class Profile extends Component {
+class About extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    
+
+      <h1></h1>
+    
+    return (
+      <p>An unstoppable coding force. They are sure to wreck the industry with their creativity and determination. When they are not slaying servers they take time to sit down and indulge in the pleasures of literature. We hope you enjoy their private selection.</p>
+    )
   }
 };
 
-export default Profile;
+export default About;
